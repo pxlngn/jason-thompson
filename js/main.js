e55afbc936fc269d889e6f920469a3eb1712a2e2
Jason Thompson <jason@pivotdesign.com>
2018-07-05 09:58:01 -0500

    progress

--- js/main.js
+++ js/main.js
@@ -2,30 +2,59 @@
 // @codekit-prepend '../bower_components/bootstrap/dist/js/bootstrap.min.js'
 // @codekit-prepend '../bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js'
 // @codekit-prepend '../bower_components/scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
+// @codekit-prepend '../bower_components/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js'
 
+// @codekit-prepend '../bower_components/gsap/src/minified/jquery.gsap.min.js'
+// @codekit-prepend '../bower_components/gsap/src/minified/TweenMax.min.js'
 
 
 
 $(document).ready(function() {
 
-  // init
-  var controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave'
-    }
-  });
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+  // fix Start
+  var pinStartScene = new ScrollMagic.Scene({
+      triggerElement: '#start',
+      triggerHook: 0,
+      duration: '35%'
+    })
+    .setPin('#start', {
+      pushFollowers: false
+    })
+    .addTo(controller);
 
-  // get all slides
-  var slides = document.querySelectorAll("section.panel");
-
-  // create scene for every slide
-  for (var i=0; i<slides.length; i++) {
-    new ScrollMagic.Scene({
-        triggerElement: slides[i]
+  // loop thru each project element
+  $('.panel').each(function() {
+    // Build Scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement: this.children[0],
+        triggerHook: 0.9,
       })
-      .setPin(slides[i])
-      .addIndicators() // add indicators (requires plugin)
+      .setClassToggle(this, 'fade-in')
+      .addIndicators({
+        name: 'fade scene',
+        colorTrigger: 'pink',
+        indent: 10,
+        colorStart: 'cyan'
+      })
       .addTo(controller);
-  }
+
+  });
+// video play code, chrome seems to require
+  (function() {
+    /**
+     * Video element
+     * @type {HTMLElement}
+     */
+    var video = document.getElementById("myVideo");
+
+    /**
+     * Check if video can play, and play it
+     */
+    video.addEventListener( "canplay", function() {
+      video.play();
+    });
+  })();
 
 });
